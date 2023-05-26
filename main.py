418bffcf4094667b3034c6214c1729f18722539b
NadVNik <128215381+NadVNik@users.noreply.github.com>
2023-05-26 21:01:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-"""sorting the list"""
 import streamlit as st
 
+
 def get_pass_list(data, save, sex):
-    """choosing who is saved"""
     if sex == 'мужчины':
         sex = 'male'
     else:
-        sex ='female'
+        sex = 'female'
     out_list = []
     for line in data:
         if line.split(',')[1] == save and line.split(',')[5] == sex:
@@ -14,11 +13,11 @@ def get_pass_list(data, save, sex):
     return out_list
 
 with open('data.csv') as file:
-    data =file.readlines()
+    data = file.readlines()
+
 
 def makarnv_code():
     sex = st.selectbox('Выберите пол пассажира:', ['мужчины', 'женщины'])
     save = st.selectbox("Спасен?", ['0', '1'])
     st.table({"Спасенные пассажиры:": get_pass_list(data, save, sex)})
-
-makarnv_code()
+    
