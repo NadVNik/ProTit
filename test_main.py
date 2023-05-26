f95463b01fa9c185efa649b1bffc2870148b0d65
NadVNik <128215381+NadVNik@users.noreply.github.com>
2023-05-26 21:55:38 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,13 +2,11 @@ from main import get_pass_list
 
 
 def test_pass_list_male():
-    data = ['2,1,1,"Cumings, Mrs. John Bradley (Florence Briggs Thayer)",female', '18,1,2,"Williams, Mr. Charles Eugene",male',
-            '2,1,1,"Cumings, Mrs. John Bradley (Florence Briggs Thayer)",female']
-    assert get_pass_list(data, 'мужчины', '1') == ['18,1,2,"Williams, Mr. Charles Eugene",male']
+    data = ['0,1,2,3,4,female', '0,1,2,3,4,male', '0,1,2,3,4,female']
+    assert get_pass_list(data, 'мужчины', '1') == ['0,1,2,3,4,male']
 
 
 def test_pass_list_female():
-    data = ['891,0,3,"Dooley, Mr. Patrick",male', '18,1,2,"Williams, Mr. Charles Eugene",male',
-            '2,1,1,"Cumings, Mrs. John Bradley (Florence Briggs Thayer)",female']
-    assert get_pass_list(data, 'женщины', '1') == ['"2,1,1,Cumings, Mrs. John Bradley (Florence Briggs Thayer)",female']
+    data = ['0,1,2,3,4,male'', '0,1,2,3,4,female', '0,1,2,3,4,male']
+    assert get_pass_list(data, 'женщины', '1') == ['0,1,2,3,4,female']
     
